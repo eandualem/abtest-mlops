ed1022a6c48cdc10172814db70823f96ba9c0adb
Elias <eandualem@gmail.com>
2021-07-24 00:30:34 +0300

    feat: add decision tree model trainer

---
+++ scripts/train_decision_trees_model.py
@@ -0,0 +1,14 @@
+import pickle
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from config import Config
+
+Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
+
+X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
+y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
+
+model = DecisionTreeClassifier(random_state=42)
+model.fit(X_train, y_train.to_numpy().ravel())
+
+pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
