f2987bbc2ecd09ca36d0de457bda6b37c9dcc033
Elias <eandualem@gmail.com>
2021-07-24 00:27:37 +0300

    feat: add python config file

---
+++ scripts/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+
+class Config:
+  RANDOM_SEED = 42
+  ASSETS_PATH = Path("../")
+  ORIGINAL_DATASET_FILE_PATH = ASSETS_PATH / "AdSmartABdata.csv"
+  DATASET_PATH = ASSETS_PATH / "data"
+  FEATURES_PATH = ASSETS_PATH / "features"
+  MODELS_PATH = ASSETS_PATH / "models"
+  METRICS_FILE_PATH = ASSETS_PATH / "metrics.json"
