151bc8f6830627f0c02b8bf72767f486455936a6
Elias <eandualem@gmail.com>
2021-07-23 19:24:45 +0300

    feat: add code from ABTesting

---
+++ scripts/__init__.py
@@ -0,0 +1,12 @@
+"""
+A Python package for scripts
+"""
+from .df_helper import DfHelper
+from .plot import *
+from .stats import stat
+import sys as _sys
+import os as _os
+
+__version__ = '0.01'
+__author__ = 'Elias Andualem'
+__email__ = 'eandualem@gmail.com'
