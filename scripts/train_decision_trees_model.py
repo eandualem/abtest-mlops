f99d95a6ef58312708af97ff46589e57082a71a2
Elias <eandualem@gmail.com>
2021-07-24 06:47:54 +0300

    feat: update the evaluation script

--- scripts/train_decision_trees_model.py
+++ scripts/train_decision_trees_model.py
@@ -11,4 +11,4 @@ y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
 model = DecisionTreeClassifier(random_state=42)
 model.fit(X_train, y_train.to_numpy().ravel())
 
-pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
+pickle.dump(model, open(str(Config.MODELS_PATH / "decision_tree_model.pickle"), "wb"))
