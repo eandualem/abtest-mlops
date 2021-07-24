dcdaca83ee1aebc77d5b4cb7807f473b9a26f231
Elias <eandualem@gmail.com>
2021-07-24 14:25:39 +0300

    feat: original data with no columns deleted

--- scripts/config.py
+++ scripts/config.py
@@ -4,8 +4,8 @@ from pathlib import Path
 class Config:
   RANDOM_SEED = 42
   ASSETS_PATH = Path("../")
-  REPO = "https://github.com/eandualem/abtest-mlops"
-  DATASET_FILE_PATH = ASSETS_PATH / "data/AdSmartABdata.csv"
+  REPO = "/Users/ea/Projects/abtest-mlops"
+  DATASET_FILE_PATH = "data/AdSmartABdata.csv"
   DATASET_PATH = ASSETS_PATH / "data"
   FEATURES_PATH = ASSETS_PATH / "features"
   MODELS_PATH = ASSETS_PATH / "models"
--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -3,19 +3,19 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import dvc.api
 import mlflow
-
 from config import Config
 
 np.random.seed(Config.RANDOM_SEED)
+version = 'v2'  # we can retrieve any version of your dataset by changing this
 
-data_url = dvc.api.get_url(path=str(Config.DATASET_FILE_PATH), repo=str(Config.REPO), rev="v2")
+data_url = dvc.api.get_url(path=str(Config.DATASET_FILE_PATH), repo=str(Config.REPO), rev=version)
 
 df = pd.read_csv(data_url, sep=',')
 
 # Log data params
-mlflow.log_params('data_url', data_url)
-mlflow.log_params('input_rows', df.shape[0])
-mlflow.log_params('input_cols', df.shape[1])
+mlflow.log_param('data_url', data_url)
+mlflow.log_param('input_rows', df.shape[0])
+mlflow.log_param('input_cols', df.shape[1])
 
 df_train, df_test = train_test_split(df, test_size=0.1, random_state=Config.RANDOM_SEED,)
 
