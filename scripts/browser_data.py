e555239179dc73a7bac6395c5b4783a4d8d1e658
Elias <eandualem@gmail.com>
2021-07-24 14:24:39 +0300

    refactor: merge data version creaters

--- scripts/browser_data.py
+++
@@ -1,7 +0,0 @@
-from df_helper import DfHelper
-
-helper = DfHelper()
-df = helper.read_csv("../data/AdSmartABdata.csv")
-df.drop('platform_os', inplace=True, axis=1)
-
-helper.save_csv(df, "../data/AdSmartABdata.csv")
--- scripts/platform_os_data.py
+++
@@ -1,7 +0,0 @@
-from df_helper import DfHelper
-
-helper = DfHelper()
-df = helper.read_csv("../data/AdSmartABdata.csv")
-df.drop('browser', inplace=True, axis=1)
-
-helper.save_csv(df, "../data/AdSmartABdata.csv")
